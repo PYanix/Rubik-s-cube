1c5c5b123c14fdf4881b9d22b8593604d123f58a
PYanix <140638821+PYanix@users.noreply.github.com>
2024-06-05 17:02:54 +0300

    Update cube_model.py

--- cube_model.py
+++ cube_model.py
@@ -4,30 +4,14 @@ from copy import deepcopy
 class Cube:
     def __init__(self, dim: int):
         self.dim = dim
-        colors = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-        def set_colors(color):
-            c = color
-            return [['1' + f'{c}', '2' + f'{c}', '3' + f'{c}'], ['4' + f'{c}', '5' + f'{c}', '6' + f'{c}'],
-                    ['7' + f'{c}', '8' + f'{c}', '9' + f'{c}']]
-
-        # colors = [['1'+f'{c}', '2'+f'{c}', '3'+f'{c}'], ['4'+f'{c}', '5'+f'{c}', '6'+f'{c}'], ['7'+f'{c}', '8'+f'{c}', '9'+f'{c}']]
-
-        self.R_colors = set_colors('R')
-        self.L_colors = set_colors('L')
-        self.U_colors = set_colors('U')
-        self.D_colors = set_colors('D')
-        self.F_colors = set_colors('F')
-        self.B_colors = set_colors('B')
-
-        """
-        self.R_colors = [['b' for _ in range(self.dim)] for _ in range(self.dim)]  # R - right face of the cube
-        self.L_colors = [['g' for _ in range(self.dim)] for _ in range(self.dim)]  # L - left face of the cube
-        self.U_colors = [['w' for _ in range(self.dim)] for _ in range(self.dim)]  # U - upper face of the cube
-        self.D_colors = [['y' for _ in range(self.dim)] for _ in range(self.dim)]  # D - down face of the cube
+        self.R_colors = [['y' for _ in range(self.dim)] for _ in range(self.dim)]  # R - right face of the cube
+        self.L_colors = [['w' for _ in range(self.dim)] for _ in range(self.dim)]  # L - left face of the cube
+        self.U_colors = [['b' for _ in range(self.dim)] for _ in range(self.dim)]  # U - upper face of the cube
+        self.D_colors = [['g' for _ in range(self.dim)] for _ in range(self.dim)]  # D - down face of the cube
         self.F_colors = [['r' for _ in range(self.dim)] for _ in range(self.dim)]  # F - front face of the cube
         self.B_colors = [['o' for _ in range(self.dim)] for _ in range(self.dim)]  # B - back face of the cube
-        """
+
 
     def __repr__(self) -> str:
         return f'''       {' '.join(map(str, self.U_colors))}'''
@@ -99,6 +83,40 @@ class Cube:
     def rotate_F_streak(self):
         for _ in range(3):
             self.rotate_F()
+
+    def rotate_B(self):
+        matrixR = deepcopy(self.R_colors)
+        matrixL = deepcopy(self.L_colors)
+        matrixU = deepcopy(self.U_colors)
+        matrixD = deepcopy(self.D_colors)
+        for i in range(3):
+            self.L_colors[2-i][0] = matrixU[0][i]
+            self.D_colors[2][i] = matrixL[i][0]
+            self.R_colors[2-i][2] = matrixD[2][i]
+            self.U_colors[0][i] = matrixR[i][2]
+        self.B_colors = [[self.B_colors[self.dim - j - 1][i] for j in range(self.dim)] for i in range(self.dim)]
+
+
+    def rotate_B_streak(self):
+        for _ in range(3):
+            self.rotate_B()
+
+    def rotate_D(self):
+        matrixR = deepcopy(self.R_colors)
+        matrixL = deepcopy(self.L_colors)
+        matrixF = deepcopy(self.F_colors)
+        matrixB = deepcopy(self.B_colors)
+        for i in range(3):
+            self.R_colors[2][i] = matrixF[2][i]
+            self.F_colors[2][i] = matrixL[2][i]
+            self.L_colors[2][2-i] = matrixB[0][i]
+            self.B_colors[0][2-i] = matrixR[2][i]
+        self.D_colors = [[self.D_colors[self.dim - j - 1][i] for j in range(self.dim)] for i in range(self.dim)]
+
+    def rotate_D_streak(self):
+        for _ in range(3):
+            self.rotate_D()
+
     def rotate_cube_U(self):
         matrixR = deepcopy(self.R_colors)
         matrixL = deepcopy(self.L_colors)
